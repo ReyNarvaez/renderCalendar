1e08aec769e87712ef5c82db040cb3d87fc36370
reynarvaez <reynaldo_narvaez@hotmail.com>
2018-03-14 18:57:30 -0400

    finished calendar rendering with weekdays, weekends and holidays

--- resources/js/app.js
+++ resources/js/app.js
@@ -18,7 +18,7 @@ jQ("#startDate").on("keydown",function(e){
 jQ("#renderBtn").on("click",function(e){
 	
 	if(isFormValid()){
-		
+
 		renderCalendar();
 	}
 });
@@ -41,4 +41,75 @@ function isFormValid(){
 function renderCalendar(){
 
 	console.log("render calendar");
+
+	var startDate = jQ("#startDate").val();
+	var numDays = jQ("#numDays").val();
+	var countryCode = jQ("#countryCode").val();
+
+	var startDate = moment(startDate);
+
+	var newCalendar = null;
+	var monthHeader = null;
+	var currentMonth = null;
+	var tbody = null;
+
+	var numMonths = Math.ceil(moment(startDate).add("days",50).diff(startDate,"months",true));
+	
+	var tr = null;
+	var td = null;
+
+	for(var count = 0; count < numMonths; count++){
+
+		newCalendar = document.getElementById("calendarContainer").cloneNode(true);
+		monthHeader = newCalendar.getElementsByClassName("monthHeader")[0];
+		monthHeader.innerHTML = startDate.format('MMMM');
+
+		currentMonth = startDate.format('MMMM');
+
+		numMonths = Math.ceil(moment(startDate).add("days",50).diff(startDate,"months",true));
+
+		tbody = newCalendar.getElementsByTagName("tbody")[0];
+
+		for(var i = 0; i < 5; i++){
+
+			tr = document.createElement("tr");
+			tr.scope = "row";
+
+			for(var j = 0; j < 7; j++){
+
+				td = document.createElement("td");			
+				td.innerHTML = "&nbsp;";
+				td.className = "invalidday";
+
+				if(j == startDate.weekday() && currentMonth == startDate.format('MMMM')){
+
+					td.innerHTML = startDate.format("DD");
+
+					if(!startDate.isHoliday()){
+
+						if(j > 0 && j < 6){
+
+							td.className = "weekday";
+						}
+						else{
+
+							td.className = "weekend";
+						}
+					}
+					else{
+
+						td.className = "holiday";
+					}
+
+					startDate.add("days",1);
+				}
+
+				tr.appendChild(td);
+			}
+
+			tbody.appendChild(tr);
+		}
+
+		document.body.appendChild(newCalendar);
+	}
 }
